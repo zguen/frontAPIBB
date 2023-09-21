336cf72b87f1d1aa7e411a411e341c9d36e6634b
zguen <guillaume.mousin@gmail.com>
2023-09-21 11:05:32 +0200

    creation categorie, saison et restriction ok

--- src/app/components/ajout-aliment/ajout-aliment.component.ts
+++ src/app/components/ajout-aliment/ajout-aliment.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Aliment } from 'src/app/models/aliment';
+import { Categorie } from 'src/app/models/categorie';
 import { NewAliment } from 'src/app/models/newAliment';
 import { Restriction } from 'src/app/models/restriction';
 import { Saison } from 'src/app/models/saison';
 import { AlimentService } from 'src/app/services/aliment.service';
+import { CategorieService } from 'src/app/services/categorie.service';
 import { RestrictionsService } from 'src/app/services/restrictions.service';
 import { SaisonsServiceService } from 'src/app/services/saisons-service.service';
 
@@ -17,12 +19,15 @@ export class AjoutAlimentComponent implements OnInit {
   saisons: Saison[] = [];
   selectedSaisons: Saison[] = [];
   restrictions: Restriction[] = [];
-  selectedRestrictions: Restriction[] = []
+  selectedRestrictions: Restriction[] = [];
+  selectedCategorie: Categorie[] = [];
+  categorie: Categorie[] = [];
 
   constructor(
     private alimentService: AlimentService,
     private saisonsService: SaisonsServiceService,
-    private restrictionsService: RestrictionsService
+    private restrictionsService: RestrictionsService,
+    private categorieService: CategorieService
   ) {}
 
   ngOnInit() {
@@ -33,11 +38,14 @@ export class AjoutAlimentComponent implements OnInit {
     this.restrictionsService.getRestrictions().subscribe((data) => {
       this.restrictions = data;
     });
+    this.categorieService.getCategories().subscribe((data) => {
+      this.categorie = data;
+    });
   }
 
   createAliment(
     libelle: string,
-    category: string,
+    category: Categorie[],
     age_introduction: number,
     saisons: Saison[],
     restrictions: Restriction[]
@@ -49,7 +57,12 @@ export class AjoutAlimentComponent implements OnInit {
       saisons: saisons,
       restrictions: restrictions,
     };
-    if (!libelle || !category || !age_introduction || this.selectedSaisons.length === 0) {
+    if (
+      !libelle ||
+      !category ||
+      !age_introduction ||
+      this.selectedSaisons.length === 0
+    ) {
       alert(`Merci de renseigner les champs vides`);
     } else {
       this.alimentService.createAliment(newAliment).subscribe((data) => {
@@ -61,7 +74,6 @@ export class AjoutAlimentComponent implements OnInit {
   }
 
   clearModel() {
-        this.selectedSaisons = [];
-    }
-
+    this.selectedSaisons = [];
+  }
 }
--- src/app/models/newAliment.ts
+++ src/app/models/newAliment.ts
@@ -1,9 +1,10 @@
+import { Categorie } from "./categorie";
 import { Restriction } from "./restriction";
 import { Saison } from "./saison";
 
 export interface NewAliment {
     libelle: string,
-    category: string,
+    category: Categorie[],
     age_introduction: number,
     saisons: Saison[],
     restrictions: Restriction[]
